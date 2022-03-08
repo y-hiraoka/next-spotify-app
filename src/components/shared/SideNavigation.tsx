56181cd76f844386da0a76be3685608d7d979077
y-hiraoka <y-hiraoka@murauchi.com>
2022-03-08 22:31:12 +0900

    SideNavigation に 保存しているプレイリストへのリンク追加

--- src/components/shared/SideNavigation.tsx
+++ src/components/shared/SideNavigation.tsx
@@ -6,10 +6,13 @@ import {
   Text,
   useColorModeValue,
   Image,
+  Divider,
+  Link,
+  SkeletonText,
 } from "@chakra-ui/react";
 import NextLink, { LinkProps as NextLinkProps } from "next/link";
 import { useRouter } from "next/router";
-import { VFC } from "react";
+import { Suspense, VFC } from "react";
 import { IconType } from "react-icons";
 import {
   MdHome,
@@ -18,13 +21,16 @@ import {
   MdOutlineLibraryMusic,
   MdSearch,
 } from "react-icons/md";
+import { useUserPlaylists } from "../../hooks/spotify-api";
+import { useSecondaryTextColor } from "../../hooks/useSecondaryTextColor";
 import { pagesPath } from "../../lib/$path";
+import { range } from "../../lib/range";
 
-const useLinkColor = (isActive: boolean) =>
-  useColorModeValue(
-    isActive ? "gray.900" : "gray.500",
-    isActive ? "gray.100" : "gray.500"
-  );
+const useLinkColor = (isActive: boolean) => {
+  const primaryColor = useColorModeValue("gray.900", "gray.100");
+  const secondaryColor = useSecondaryTextColor();
+  return isActive ? primaryColor : secondaryColor;
+};
 
 export const SideNavigation: VFC = () => {
   const router = useRouter();
@@ -33,7 +39,7 @@ export const SideNavigation: VFC = () => {
   const libraryIsActive = router.pathname === pagesPath.library.$url().pathname;
 
   return (
-    <Box h="full" bgColor={useColorModeValue(undefined, "gray.900")} p="5">
+    <Box h="full" w="60" bgColor={useColorModeValue(undefined, "gray.900")} p="5">
       <NextLink href={pagesPath.$url()} passHref>
         <Box as="a">
           <Image
@@ -47,7 +53,7 @@ export const SideNavigation: VFC = () => {
           />
         </Box>
       </NextLink>
-      <Stack marginTop="8" spacing="3">
+      <Stack marginTop="8" spacing="4">
         <NavigationLink
           label="Home"
           href={pagesPath.$url()}
@@ -67,6 +73,10 @@ export const SideNavigation: VFC = () => {
           isActive={libraryIsActive}
         />
       </Stack>
+      <Divider my="4" />
+      <Suspense fallback={<PlaylistLinksFallback />}>
+        <PlaylistLinks />
+      </Suspense>
     </Box>
   );
 };
@@ -85,11 +95,56 @@ const NavigationLink: VFC<{
         transition="color 0.2s ease"
         _hover={{ color: useColorModeValue("gray.900", "gray.100") }}
       >
-        <Icon as={icon} fontSize="2xl" />
-        <Text as="span" fontSize="sm" fontWeight={isActive ? "bold" : undefined}>
+        <Icon as={icon} fontSize="3xl" />
+        <Text as="span" fontWeight={isActive ? "bold" : undefined}>
           {label}
         </Text>
       </HStack>
     </NextLink>
+  );
+};
+
+const PlaylistLinks: VFC = () => {
+  const { data: playlists } = useUserPlaylists([]);
+
+  return (
+    <Stack>
+      {playlists?.items.map((playlist) => (
+        <PlaylistLink key={playlist.id} playlist={playlist} />
+      ))}
+    </Stack>
+  );
+};
+
+const PlaylistLink: VFC<{
+  playlist: SpotifyApi.PlaylistObjectSimplified;
+}> = ({ playlist }) => {
+  const router = useRouter();
+  const href = pagesPath.playlists._playlistId(playlist.id).$url();
+
+  const isActive =
+    router.pathname === href.pathname &&
+    router.query.playlistId === href.query.playlistId;
+
+  return (
+    <NextLink href={href} passHref>
+      <Link
+        noOfLines={1}
+        color={useLinkColor(isActive)}
+        fontWeight={isActive ? "bold" : undefined}
+      >
+        {playlist.name}
+      </Link>
+    </NextLink>
+  );
+};
+
+const PlaylistLinksFallback: VFC = () => {
+  return (
+    <Stack spacing="4">
+      {[...range(0, 5)].map((i) => (
+        <SkeletonText key={i} noOfLines={1} />
+      ))}
+    </Stack>
   );
 };
