296655b0bed8b27102b7ed393da38c47a3e29534
y-hiraoka <y-hiraoka@murauchi.com>
2022-03-13 13:37:17 +0900

    metaタグ挿入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,26 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { chakraTheme } from "../common/chakra-theme";
 import { SpotifyClientProvider } from "../hooks/spotify-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={chakraTheme}>
-      <SpotifyClientProvider>
-        <Component {...pageProps} />
-      </SpotifyClientProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Spotify Clone App</title>
+        <meta name="description" content="Complete Spotify clone app." />
+        <meta property="og:description" content="Complete Spotify clone app." />
+        <meta property="og:title" content="Spotify Clone App" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Spotify Clone App" />
+      </Head>
+      <ChakraProvider theme={chakraTheme}>
+        <SpotifyClientProvider>
+          <Component {...pageProps} />
+        </SpotifyClientProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
